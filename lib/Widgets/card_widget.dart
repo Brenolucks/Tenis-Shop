7939ab27339dad80d217c05183472cce16ec39a6
Brenolucks <brenolucascontato@gmail.com>
2022-08-17 00:16:57 -0300

    Finishing cards, adding images

--- lib/Widgets/card_widget.dart
+++ lib/Widgets/card_widget.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'dart:math' as math;
 
 class CardWidget extends StatelessWidget {
-  const CardWidget({Key? key}) : super(key: key);
+  final String categoryShoes;
+  final String nameShoes;
+  final String imageSrc;
+  final Color colorOptionOne, colorOptionTwo, colorOptionThree;
+  final String price;
+
+  const CardWidget({
+    Key? key,
+    required this.categoryShoes,
+    required this.nameShoes,
+    required this.imageSrc,
+    required this.colorOptionOne,
+    required this.colorOptionTwo,
+    required this.colorOptionThree,
+    required this.price,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,48 +30,87 @@ class CardWidget extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Unissex Shoes'),
-          Text('Balenciaga Speed'),
-          Transform.rotate(
-            angle: -math.pi / 25,
+          Container(
+            padding: EdgeInsets.only(left: 10, top: 15),
+            child: Text(
+              categoryShoes,
+              style: GoogleFonts.montserrat(
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 10, top: 10),
+            child: Text(
+              nameShoes,
+              style: GoogleFonts.montserrat(
+                  fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10.0),
             child: Image.asset(
-              'assets/images/balenciaga_shoes.jpg',
+              imageSrc,
               fit: BoxFit.contain,
-              width: 127,
+              width: 120,
             ),
           ),
           Row(
             children: [
-              CircleAvatar(
-                backgroundColor: Colors.black,
-                maxRadius: 10,
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: CircleAvatar(
+                  backgroundColor: colorOptionOne,
+                  maxRadius: 5,
+                ),
               ),
-              CircleAvatar(
-                backgroundColor: Colors.black,
-                maxRadius: 10,
+              Padding(
+                padding: const EdgeInsets.only(left: 5.0),
+                child: CircleAvatar(
+                  backgroundColor: colorOptionTwo,
+                  maxRadius: 5,
+                ),
               ),
-              CircleAvatar(
-                backgroundColor: Colors.black,
-                maxRadius: 10,
+              Padding(
+                padding: const EdgeInsets.only(left: 5.0),
+                child: CircleAvatar(
+                  backgroundColor: colorOptionThree,
+                  maxRadius: 5,
+                ),
               ),
             ],
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Text('\$452'),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  price,
+                  style: GoogleFonts.montserrat(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 26,
+                  ),
+                ),
+              ),
               Container(
-                width: 50,
-                height: 50,
+                width: 45,
+                height: 51.5,
                 decoration: BoxDecoration(
                   color: Colors.deepOrange,
-                  borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                  ),
                 ),
-                child: Icon(
-                  Icons.add,
-                  color: Colors.white,
+                child: Center(
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: 25,
+                  ),
                 ),
-              )
+              ),
             ],
           ),
         ],
