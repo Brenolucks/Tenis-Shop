86264952b347858bec1d42835d17f11bf6275d93
Brenolucks <brenolucascontato@gmail.com>
2022-08-18 23:51:50 -0300

    Creating mockup for page of shoes selected

--- lib/Widgets/shoes_page_widget.dart
+++ lib/Widgets/shoes_page_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'dart:math' as math;
 
 class ShowPageWidget extends StatelessWidget {
   const ShowPageWidget({Key? key}) : super(key: key);
@@ -20,10 +22,133 @@ class ShowPageWidget extends StatelessWidget {
                 width: 370,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
-                  color: Colors.grey,
+                  color: Colors.grey[200],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.arrow_back_ios_new),
+                          iconSize: 24,
+                          onPressed: () {
+                            Navigator.pop(context, true);
+                          },
+                        ),
+                        Text(
+                          'Unissex Shoes',
+                          style: GoogleFonts.montserrat(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.deepOrange,
+                          ),
+                        ),
+                        Icon(
+                          Icons.favorite_border,
+                          size: 24,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      child: Transform.rotate(
+                        angle: -math.pi / 30,
+                        child: Image.asset(
+                          'assets/images/puma_rsx.png',
+                          fit: BoxFit.contain,
+                          width: 300,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            )
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Puma RS-X Toys',
+                  style: GoogleFonts.montserrat(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text('Description\n'),
+                Text(
+                  'Just when you thought you\'d seen it all, Nike wows \n'
+                  'with an all-new way to quickly and easily get into \n'
+                  'your shoes. The entire heel (including t...',
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Color'),
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.yellow,
+                      maxRadius: 5,
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Colors.red,
+                      maxRadius: 5,
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Colors.greenAccent,
+                      maxRadius: 5,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: [
+                Text('Size'),
+                //Carousel
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.amber,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.purple,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
       ),
